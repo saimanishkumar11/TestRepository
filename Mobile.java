fa0b9b01c91b3f3c2d0adf7e1dbd69e1f49e164c
saimanishkumar11 <106506216+saimanishkumar11@users.noreply.github.com>
2022-05-31 11:16:15 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,12 @@
+class Mobile
+{
+public static void main(String[] args)
+{
+int mobile_cost=15000;
+String model_name="Redmi";
+String mobile_colour="Black";
+System.out.println("Mobile cost is "+mobile_cost);
+System.out.println("Model name is "+model_name);
+System.out.println("Mobile colour is "+ mobile_colour);
+}
+}
